b7917fbf599a529f16f8bd9ead6d9abafb45d28d
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-03-26 16:11:21 -0400

    finished routing and logic for main post and footer posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @post = @posts.max_by { |p| p.created_at }
+    @posts -= [@post]
   end
 
   def show
     @posts = Post.all
-    @post = @posts.detect{ |p| p.id == params[:id].to_i }
+    @post = @posts.detect { |p| p.id == params[:id].to_i }
+    @posts -= [@post]
     render index
   end
 
