cdf5c4c67186766a897a12ecb2b74607f2fca4b5
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-03-26 18:37:11 -0400

    posts are now sorted and displayed by date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @posts = Post.all
+    @posts = Post.sorted
     @post = Post.find(params[:id].to_i)
     @posts -= [@post]
     render 'index'
--- app/models/post.rb
+++ app/models/post.rb
@@ -149,5 +149,8 @@ class Post
     all.detect { |post| post.id == id }
   end
 
+  def self.sorted
+    all.sort_by { |post| Date.today - post.created_at.to_i }
+  end
 
 end
