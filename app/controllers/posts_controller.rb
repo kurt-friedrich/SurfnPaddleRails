866e8d43dd1dbb3c21ad36683dd4044de9ae925e
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-03-30 16:03:10 -0400

    added new, create methods and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,40 @@ class PostsController < ApplicationController
     @posts -= [@post]
   end
 
+  def new
+   posts
+   @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
   def show
     posts
     @post = Post.find(params[:id].to_i)
     @posts -= [@post]
     render 'index'
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :author_id)
   end
 
 end
